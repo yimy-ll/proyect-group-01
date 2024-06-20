cd6993e2aaba60aab4d1b05c54285a27616fb3fc
yimy llanos <llanosyimy@gmail.com>
2024-06-20 14:40:15 -0300

    Corregir exportación de modelos y agregar asociaciones entre Usuario, Producto y Comentario
    - Se resolvieron problemas con la exportación de modelos.
    - Se agregaron asociaciones entre Usuario y Producto.
    - Se agregaron asociaciones entre Usuario y Comentario.

--- database/models/Producto.js
+++ database/models/Producto.js
@@ -1,42 +1,45 @@
-let alias = "Producto";
+module.exports = (sequelize, dataTypes) => {
+  let alias = "Producto";
 
-let cols = {
-  id: {
-    autoIncrement: true,
-    primaryKey: true,
-    type: dataTypes.INTEGER,
-  },
-  imagenProducto: {
-    type: dataTypes.STRING,
-  },
-  nombreProducto: {
-    type: dataTypes.STRING,
-  },
-  descripcionProducto: {
-    type: dataTypes.STRING,
-  },
-  createdAt: {
-    type: dataTypes.DATE,
-  },
-  updatedAt: {
-    type: dataTypes.DATE,
-  },
-  deletedAt: {
-    type: dataTypes.DATE,
-  },
+  let cols = {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: dataTypes.INTEGER,
+    },
+    imagenProducto: {
+      type: dataTypes.STRING,
+    },
+    nombreProducto: {
+      type: dataTypes.STRING,
+    },
+    descripcionProducto: {
+      type: dataTypes.STRING,
+    },
+    createdAt: {
+      type: dataTypes.DATE,
+    },
+    updatedAt: {
+      type: dataTypes.DATE,
+    },
+    deletedAt: {
+      type: dataTypes.DATE,
+    },
+  };
+
+  let config = {
+    tableName: "productos",
+    timestamps: true,
+    underscored: true,
+  };
+
+  let Producto = sequelize.define(alias, cols, config); //estos tres son parametros
+
+  Producto.associate = function (models) {
+    Producto.belongsTo(models.Usuario, {
+      as: "usuario",
+      foreignKey: "id_usuario",
+    });
+  };
+  return Producto;
 };
-
-let config = {
-  tableName: "productos",
-  timestamps: true,
-  underscored: true,
-};
-
-let Producto = sequelize.define(alias, cols, config); //estos tres son parametros
-
-Producto.belongsTo(models.Usuario, {
-  as: "usuario",
-  foreignKey: "id_usuario",
-});
-
-return Producto;
--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -1,55 +1,55 @@
-let alias = "Usuario"
+module.exports = (sequelize, dataTypes) => {
+  let alias = "Usuario";
 
-    let cols = {
-        id:{
-            autoIncrement: true,
-            primaryKey: true,
-            type: dataTypes.INTEGER,
-        },
-        email:{
-            type: dataTypes.STRING
-        },
-        contrasenia:{
-            type: dataTypes.STRING
-        },
-        fecha:{
-            type: dataTypes.DATE
-        },
-        dni:{
-            type: dataTypes.INTEGER
-        },
-        fotoPerfil:{
-            type: dataTypes.STRING
-        },
-        createdAt:{
-            type: dataTypes.DATE
-        },
-        updatedAt:{
-            type: dataTypes.DATE
-        },
-        deletedAt:{
-            type: dataTypes.DATE
-        },
+  let cols = {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: dataTypes.INTEGER,
+    },
+    email: {
+      type: dataTypes.STRING,
+    },
+    contrasenia: {
+      type: dataTypes.STRING,
+    },
+    fecha: {
+      type: dataTypes.DATE,
+    },
+    dni: {
+      type: dataTypes.INTEGER,
+    },
+    fotoPerfil: {
+      type: dataTypes.STRING,
+    },
+    createdAt: {
+      type: dataTypes.DATE,
+    },
+    updatedAt: {
+      type: dataTypes.DATE,
+    },
+    deletedAt: {
+      type: dataTypes.DATE,
+    },
+  };
 
-    }
+  let config = {
+    tableName: "usuarios",
+    timestamps: true,
+    underscored: true,
+  };
 
-    let config ={
-        tableName: "usuarios",
-        timestamps: true,
-        underscored: true,
+  let Usuario = sequelize.define(alias, cols, config); //estos tres son parametros
+  Usuario.associate = function (models) {
+    Usuario.hasMany(models.Comentario, {
+      as: "comentarios",
+      foreignKey: "usuarioId",
+    });
+    Usuario.hasMany(models.Producto, {
+      as: "productos",
+      foreignKey: "usuarioId",
+    });
+  };
 
-    }
-
-    let Usuario = sequelize.define(alias, cols, config) //estos tres son parametros 
-    Usuario.associate = function (models) {
-        Usuario.hasMany(models.Comentario, {
-          as: "comentarios",
-          foreignKey: "usuarioId",
-        });
-        Usuario.hasMany(models.Producto, {
-          as: "productos",
-          foreignKey: "usuarioId",
-        });
-      };
-
-    return Usuario 
+  return Usuario;
+};
