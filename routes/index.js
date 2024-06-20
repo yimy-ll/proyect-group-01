216294f66a930945a90f3f0753de0272df6542d5
yimy llanos <llanosyimy@gmail.com>
2024-06-20 16:44:51 -0300

    se resolvio la view de index

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
