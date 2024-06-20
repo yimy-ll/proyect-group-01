96f064b2bab99278661b1cf39750fb9ed92fcbac
yimy llanos <llanosyimy@gmail.com>
2024-06-20 18:33:56 -0300

    cambio solo para mi base de datos

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,23 @@
-module.exports=
-{
-  "development": {
-    "username": "root",
-    "password": "yiMY1556",
-    "database": "database_development",
-    "host": "127.0.0.1",
-      "dialect": "mysql",
-      "port": 3309
+module.exports = {
+  development: {
+    username: "root",
+    password: "root",
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
